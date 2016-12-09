9d43468faff2323c58912996d27018932b08bf23
saulflores95 <saulpolo95@gmail.com>
2016-12-08 17:43:24 -0800

    Fixed stuck on load

--- client/doctors/DoctorDetail.jsx
+++ client/doctors/DoctorDetail.jsx
@@ -14,7 +14,7 @@ export default class DoctorDetail extends TrackerReact(React.Component){
 
     this.state = {
       subscription: {
-        doctors: Meteor.subscribe("userDoctors")
+        doctors: Meteor.subscribe("allDoctors")
       }
     }
   }
@@ -37,14 +37,14 @@ export default class DoctorDetail extends TrackerReact(React.Component){
 
       return(
         <div style={styles.divContainer}>
-        <Container>
-          <Row>
-            <Col xs={12} sm={12} md={12} lg={12}><DoctorHeader id={this.props.id} /></Col>
-            <Col xs={12} sm={12} md={12} lg={12}><DoctorDescription id={this.props.id} /></Col>
-            <Col xs={12} sm={6} md={6} lg={6}><DoctorContactForm id={this.props.id} /></Col>
-            <Col xs={12} sm={6} md={6} lg={6}><DoctorMap id={this.props.id} /></Col>
-          </Row>
-        </Container>
+          <Container>
+            <Row>
+              <Col xs={12} sm={12} md={12} lg={12}><DoctorHeader id={this.props.id} /></Col>
+              <Col xs={12} sm={12} md={12} lg={12}><DoctorDescription id={this.props.id} /></Col>
+              <Col xs={12} sm={6} md={6} lg={6}><DoctorContactForm id={this.props.id} /></Col>
+              <Col xs={12} sm={6} md={6} lg={6}><DoctorMap id={this.props.id} /></Col>
+            </Row>
+          </Container>
         </div>
 
       )
